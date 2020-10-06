605ecd17590f6468bb6646fbfff2712dc79bba6f
uk6900 <52121263+uk6900@users.noreply.github.com>
2020-10-06 14:59:23 +0530

    Add files via upload

---
+++ Stud.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="assets/css/ccss.css" type="text/css"
+	rel="stylesheet" />	
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap" rel="stylesheet">
+    <h1><center><b>NIHM</b></center></h1>
+    
+</head>
+<body>
+<div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <a class="navbar-brand" href="#">NIHM</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Courses</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Payment gateway</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+    </ul>
+    <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+  <div class="col-12 col-md-3 col-xl-2 bd-sidebar">
+          <form class="bd-search d-flex align-items-center">
+  <span class="algolia-autocomplete" style="position: relative; display: inline-block; direction: ltr;">
+  </div>
+</nav>
+</div>
+<form method="post" action="dummy.php" enctype="multipart/form-data">
+<div class="container">
+<table border="1" style="padding:10px">
+
+<tr>
+
+<Td>Upload  Video</td></tr>
+
+<Tr><td><input type="file" name="fileToUpload"/></td></tr>
+
+<tr><td>
+
+<input type="submit" value="Uplaod Video" name="upd"/>
+
+<input type="submit" value="Display Video" name="disp"/>
+
+</td></tr>
+
+</table>
+
+</form>
+</div>
+</div>
+
+</body>
+</html>
